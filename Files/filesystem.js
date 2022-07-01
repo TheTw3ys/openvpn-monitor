9d46bba6c4e4d6265120fb280266e845c5c2b407
Jonathan Paul <netsky@kubernetes.main>
2022-07-01 13:33:43 +0200

    added simple functions to filesystem.js

--- Files/filesystem.js
+++ Files/filesystem.js
@@ -1,1 +1,36 @@
-const
+const { renameSync, unlinkSync, appendFileSync } = require("fs");
+const { getSystemErrorMap } = require("util");
+
+function rename_file(file, renamed_file) {
+    try {
+        renameSync(file, renamed_file)
+        console.log(`File renamed to ${renamed_file}!`);
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+function delete_file(file) {
+    try {
+        unlinkSync(file)
+        console.log(`File ${file} was deleted.`)
+    } catch (error){
+        console.log(error)
+    }
+
+}
+function create_file(filename, content) {
+   try {
+    appendFileSync(filename, content)
+    console.log(`File ${filename}! created with content:\n`);
+   } catch (error) {
+    console.log(error)
+   } 
+}
+
+let file = "./Files/text.txt";
+/*Be aware that if started from other folders "text.txt" is not the same*/
+let renamed_file = "./Files/hello.txt";
+
+rename_file(renamed_file, file)
+rename_file(file, renamed_file)

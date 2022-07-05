da53ecc803480f65a365e47b228bf35ddc52b61f
Jonathan Paul <netsky@kubernetes.main>
2022-07-05 11:09:54 +0200

    type static now contains now clients

--- test.ts
+++ test.ts
@@ -1,24 +1,33 @@
 import fs from "fs";
 import { type } from "os";
+import { resourceLimits } from "worker_threads";
 
 type Header = {
   // the header of the table
   header?: Array<string>;
 };
 
-type OnlineClient = {   // a single client  
+type OnlineClient = {
+  // a single client
   realIPV4Address: string;
   commonName: string;
-  connectedSince: Date;
-  bytesReceived: number;
-  bytesSend: number;
+  connectedSince: Date | null;
+  bytesReceived: number | null;
+  bytesSent: number | null;
+  LastReference: Date | "Now";
 };
 
-type TResult = {
-  /* what you want to get back */ updatedAt: Date;
-  clients: Array<OnlineClient>;
+type TState = {
+  updatedAt: Date;
+  clients: { [id: string]: OnlineClient };
   //header: Header;
 };
+
+const state: TState = {
+  updatedAt: new Date(),
+  clients: {},
+};
+
 function get_lines(
   lines: Array<string>,
   min: number,
@@ -33,84 +42,80 @@ function get_lines(
   return end_lines;
 }
 
-function praseStuff(lines: Array<string>): TResult {
-  console.log(lines);
+function getWorkLines(lines: Array<string>) {
   const start_online: number = lines.indexOf("OpenVPN CLIENT LIST");
   const start_offline: number = lines.indexOf("ROUTING TABLE");
 
-  function get_online() {
+  function getOnlineWorkLines(): Array<string> {
     for (let x = 0; x < lines.length; x++) {
       if (x == start_online) {
         if (lines[x + 2].startsWith("Common")) {
-          const clientstart = lines.indexOf(lines[x + 3]);
-          const clientend = lines.indexOf("ROUTING TABLE") - 1;
-          const work_lines = get_lines(lines, clientstart, clientend + 1);
-          return work_lines;
+          const indexstart = lines.indexOf(lines[x + 3]);
+          const indexend = lines.indexOf("ROUTING TABLE") - 1;
+          const worklines = get_lines(lines, indexstart, indexend + 1);
+          return worklines;
         }
       }
     }
+    return [];
   }
-  function get_offline() {
+
+  function getOfflineWorkLines() {
     for (let x = 0; x < lines.length; x++) {
       if (x == start_offline) {
         if (lines[x + 1].startsWith("Virtual")) {
-          const offline_start = lines.indexOf(lines[x + 2]);
-          const offline_end = lines.indexOf("GLOBAL STATS");
-          const offline_lines: Array<string> = get_lines(
+          const indexstart = lines.indexOf(lines[x + 2]);
+          const indexend = lines.indexOf("GLOBAL STATS");
+          const worklines: Array<string> = get_lines(
             lines,
-            offline_start,
-            offline_end
+            indexstart,
+            indexend
           );
-
-          return offline_lines;
+          return worklines;
         }
       }
     }
+    return [];
   }
-  const online_member: any = get_online();
-  var offline_member: any = get_offline();
 
-  function get_values(array: Array<string>) {
-    const list: any = []
-    for (let x = 0; x < array.length; x++) {
-      const string = array[x].split(","); //no sort() because of sent and received bytes
-      list.push(string);
+  const online_members: Array<string> = getOnlineWorkLines();
+  const offline_members: Array<string> = getOfflineWorkLines();
+
+  offline_members.forEach((member: string) => {
+    const details = member.split(",");
+    const name = details[1];
+
+    state.clients[name] = {
+      bytesReceived: parseInt(details[2]),
+      commonName: details[1],
+      bytesSent: 0,
+      connectedSince: new Date(),
+      LastReference: new Date(),
+      realIPV4Address: "",
+    };
+  });
+
+  online_members.forEach((member: string) => {
+    const details = member.split(",");
+    const name = details[0];
+
+    const onlineClient = {
+      bytesReceived: parseInt(details[2]),
+      commonName: details[0],
+      bytesSent: parseInt(details[3]),
+      connectedSince: new Date(details[4]),
+      LastReference: new Date(),
+      realIPV4Address: details[1],
+    };
+    if (state.clients[name] != null) {
+      state.clients[name] = { ...state.clients[name], ...onlineClient }; //overwrite in case of key being used
+    } else {
+      state.clients[name] = onlineClient;
     }
-    return list
-  }
-  /*TODO: find out why string is not pushable to list when not type of any*/
-  const online = get_values(online_member);
-  const offline = get_values(offline_member);
-
-function get_clients(values): TResult{
- //first idea
-  function get_online_clients(value): OnlineClient {
-    var date =  new Date(value[3]).toLocaleTimeString()
-    const client: OnlineClient = [
-      value[0], //IPV4
-      value[4], //common name
-      date,
-      parseInt(value[1]),
-      parseInt(value[2])
-    ]
-    return client
-  }
-
-  clients = []
-  return clients;
-}
-get_clients(online);
-return {
-  updatedAt: new Date(),
-  clients: [],
-};
+    console.log(state);
+  });
 }
 
-const content = fs.readFileSync("./Files/vpn-status.log", "utf-8").trim(); // trimming in case of spaces
-//console.log(content); //string content
-const lines = content.split("\n"); //Array content
-//console.log(lines);
-console.log(praseStuff(lines));
-//console.log(lines.values)
+
 var d = new Date("Thu Jun 30 10:01:46 2022");
 console.log(d.toLocaleTimeString());
